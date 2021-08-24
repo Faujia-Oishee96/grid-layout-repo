7c27ada363a06a7f06787b557ba30b06301389b0
Faujia-Oishee96 <faujiaakhteroishee@gmail.com>
2021-08-24 10:58:09 +0600

    first commit

---
+++ jaan.js
@@ -0,0 +1,16 @@
+
+
+var eggPrice = 10;
+var penPrice = 5;
+
+var add = eggPrice + penPrice;
+var substraction = eggPrice - penPrice;
+var multiplication = eggPrice * penPrice;
+var division = eggPrice / penPrice;
+
+
+console.log(add);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+
---
+++ math.js
@@ -0,0 +1,46 @@
+
+// if else condition
+
+// var age = 20;
+
+// if (age < 20){
+//       console.log("OK")
+// }
+// else{
+//     console.log("Not ok")
+// }
+
+
+
+// var age = 30;
+// var money = 20000;
+
+// if(age == 30 || money == 200000){
+//     console.log("agreement done")
+// }
+// else{
+//     console.log("No agrement")
+// }
+
+
+// loop
+
+// while loop
+
+// var age = 0;
+
+// while(age<10){
+//     console.log(age)
+
+//     console.log('Baby')
+    
+//     age++;
+    
+// }
+
+// for loop
+ 
+for(var age = 0; age < 10; age++){
+    console.log(age)
+    console.log("baby")
+}
